9e547db0eafadc8167e213b3a5eaa57de9514cc1
jamesmungall <peanuts@jamesmungall.co.uk>
2015-05-29 09:16:00 +0100

    Delete addingColumn.R

--- p14-16 Select Cols, Iris/addingColumn.R
+++
@@ -1,7 +0,0 @@
-addingColumn<-function(){
-# To add a column of the ratio of sepal length to sepal width use
-#
-# msiris$sepalRatio<-msiris$Sepal.Length/msiris$Sepal.Width
-#
-# (msiris is my sample,size 10, of the iris dataframe)
-}
