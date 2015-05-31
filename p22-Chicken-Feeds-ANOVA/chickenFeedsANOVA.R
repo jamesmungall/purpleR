1cc78549436357be6b45a48c51804603ec9904c5
jamesmungall <peanuts@jamesmungall.co.uk>
2015-05-31 21:03:33 +0100

    chicken feeds ANOVA

--- p22-Chicken-Feeds-ANOVA/chickenFeedsANOVA.R
+++ p22-Chicken-Feeds-ANOVA/chickenFeedsANOVA.R
@@ -1,1 +1,28 @@
+chickenFeedsANOVA<-function(){
+  # Chicken Feeds - ANOVA           p22, purple book 31.5.15
+  #----------------------
+  # 
+  # ref: W&W pg 325 and built-in data set 'chickwts'
+  # 
+  # Ho: Chicken weights are not affected by feed type
+  #
+  #                                       feed type
+  # weight(g)   casein    horsebean   linseed   meatmeal    soybean   sunflower
+  #             368       179         309       325         243       423
+  #             .         .           .         .           .         .
+  #----------------------------------------------------------------------------
+  # n           (a)
+  # mean
+  # SS
+  #----------------------------------------------------------------------------
+  # Source        SS    df    MS    F   p-value
+  # between
+  # treatments    
+  # within
+  # (error)
+  #----------------------------------------------------------------------------
+  # 
+  # To get the data into this format
+  
+}
 
