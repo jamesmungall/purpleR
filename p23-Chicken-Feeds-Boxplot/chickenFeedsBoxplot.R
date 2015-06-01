417a7568c68fe71e76ebce9a48823f9a4914ce59
jamesmungall <peanuts@jamesmungall.co.uk>
2015-06-01 21:09:44 +0100

    chicken feeds boxplot

--- p23-Chicken-Feeds-Boxplot/chickenFeedsBoxplot.R
+++ p23-Chicken-Feeds-Boxplot/chickenFeedsBoxplot.R
@@ -1,1 +1,19 @@
+chickenFeedsBoxPlot<-function(){
+# Chicken Feeds - Boxplot           p23, purple book 1.6.15
+#----------------------
+# 
+# ref: http://www.cookbook-r.com/Graphs/Box_plot/
+# 
+# We can clearly show that the chicken weights are 
+# affected by feed type by drawing boxplots. Data is of the form
+#
+#     weight    feed
+#     --------------
+#     179       horsebean       71obs, with 6 feed factors
+#
+# To draw boxplots, use boxplot( dataValuesColumn ~ variables, dataframe)
+# In this case;
+boxplot(weight ~ feed, chickwts);
 
+
+}
